a8a1fad0daa31c78322c7d76d2d6c73daa33781b
JustAmalll <amalkrasavchik@gmail.com>
2023-04-04 13:02:00 +0600

    initial commit / graphQL setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("com.apollographql.apollo3").version("3.7.3")
+    id("dagger.hilt.android.plugin")
+    kotlin("kapt")
+}
+
+apollo {
+    service("service") {
+        packageName.set("dev.amal")
+    }
+}
+
+android {
+    namespace = "dev.amal.graphqlcountriesapp"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "dev.amal.graphqlcountriesapp"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.4"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.compose.material:material:1.4.0")
+
+    // GraphQL
+    implementation("com.apollographql.apollo3:apollo-runtime:3.7.3")
+
+    // Jetpack compose
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
+
+    implementation("com.google.dagger:hilt-android:2.45")
+    kapt("com.google.dagger:hilt-android-compiler:2.45")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
